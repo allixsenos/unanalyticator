94ec744218a248e4cbaad9f69ad7faf6289cf6f9
Luka Kladaric <allixsenos@gmail.com>
2011-10-09 19:55:09 +0200

    fix the 'trailing &s' issue

--- unanalyticator.js
+++ unanalyticator.js
@@ -1,8 +1,10 @@
 // based on Unburner source code by Zachary Halbrecht https://github.com/zackola/unburner
 // with commits from zackola and Greg Rundlett
 
-var r = /utm_[^=]+=[^&]+/gi;
+var r = /utm_[^=]+=[^&]+&?/gi;
 var url = document.location.href;
 if (url.match(r)) {
-  location.href = url.replace(r, '');
+    url = url.replace(r, ''); // kill utm_ params
+    url = url.replace(/\?&*$/i, ''); // kill trailing ?&'s
+    location.href = url;
 }
